e206ea77bb3520d20984348485e034429b29d075
Orioha Chikezirim <oriohac@gmail.com>
2022-06-22 17:20:42 +0100

    Started Javascript

---
+++ FrontEnd/HTML_CSS_JS/classwork/js/conditional.js
@@ -0,0 +1,41 @@
+// let isloggedin = true;
+// let goat = 'ronaldo';
+
+
+
+
+// if (goat == 'ronaldo') {
+//     console.log("Ronaldo is the best");
+// }else{
+//     console.log('youre deceiving yourself ');
+// }
+// let username = 'tolu';
+// let password = 'secret';
+ 
+// if (username == '') {
+//     console.log('all fields are required');
+// } else if(username == 'tolu' && password == 'secret') {
+//    console.log('login'); 
+// }else{
+//     console.log('i dont know you');
+// }
+// let num = 0
+// for (let i = 0; i < 11; i++) {
+    
+//     num += i
+// }
+// console.log(num);
+
+// let i = 1;
+// while (i <= 10) {
+    
+//     console.log(i);
+//     i++;
+    
+// }
+
+function getName() {
+   console.log('chike');
+}
+
+getName();
---
+++ FrontEnd/HTML_CSS_JS/classwork/js/life.js
@@ -0,0 +1,19 @@
+let Goat = "ronaldo";
+let bestMusicianinNigeria = "Davido";
+var name = 'tolu';
+const password = 'cdschike';
+let z = Goat.slice(1,6);
+
+// console.log(Goat);
+// console.log(bestMusicianinNigeria);
+// console.log(z);
+
+// let message = 'kola is a girl';
+
+// let result = message.replace('kola','tolu');
+// console.log(result);
+// let city = '    lagos  ';
+// console.log(city.trim());
+//  let loggedin = null;
+// let x  
+// console.log(x);
