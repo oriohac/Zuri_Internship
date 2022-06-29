d3118b210e34987fed5097614d4222790ffbbc9d
Orioha Chikezirim <oriohac@gmail.com>
2022-06-29 05:21:14 +0100

    Practiced Django REST Framework

---
+++ python/personal/Django-REST-Framework/mySite/blog/views.py
@@ -0,0 +1,19 @@
+from turtle import title
+from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer
+
+# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+# Payload ={
+#     "title":"welcome2",
+#     "body":"this is a post2"
+# }
+
+# Post.objects.create(title=Payload['title'],body=Payload['body'])
